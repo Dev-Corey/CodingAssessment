e5256e6951c0f0894a94e04cfd32b71fadbdfc32
CBoensch <cboensch6505@gmail.com>
2021-11-14 12:41:26 -0500

    Code Assessment Challenge

---
+++ assets/js/script.js
@@ -0,0 +1,231 @@
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
+const result_box = document.querySelector(".result_box");
+const option_list = document.querySelector(".option_list");
+const time_line = document.querySelector("header .time_line");
+const timeText = document.querySelector(".timer .time_left_txt");
+const timeCount = document.querySelector(".timer .timer_sec");
+
+let timeValue =  60;
+let que_count = 0;
+let que_numb = 1;
+let userScore = 0;
+let counter;
+let widthValue = 0;
+const restart_quiz = result_box.querySelector(".buttons .restart");
+const quit_quiz = result_box.querySelector(".buttons .quit");
+
+start_btn.onclick = ()=>{
+    info_box.classList.add("activeInfo"); 
+}
+
+exit_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo"); 
+}
+
+continue_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo");
+    quiz_box.classList.add("activeQuiz");
+    showQuetions(0);
+    queCounter(1);
+    startTimer(60);
+}
+
+restart_quiz.onclick = ()=>{
+    quiz_box.classList.add("activeQuiz");
+    result_box.classList.remove("activeResult");
+    timeValue = 60;
+    que_count = 0;
+    que_numb = 1;
+    userScore = 0;
+    widthValue = 0;
+    showQuetions(que_count);
+    queCounter(que_numb);
+    clearInterval(counter);
+    startTimer(timeValue);
+    timeText.textContent = "Time Left";
+    next_btn.classList.remove("show");
+}
+
+quit_quiz.onclick = ()=>{
+    window.location.reload();
+}
+const next_btn = document.querySelector("footer .next_btn");
+const bottom_ques_counter = document.querySelector("footer .total_que");
+
+next_btn.onclick = ()=>{
+    if(que_count < questions.length - 1){
+        que_count++;
+        que_numb++;
+        showQuetions(que_count);
+        queCounter(que_numb);
+        timeText.textContent = "Time Left";
+        next_btn.classList.remove("show");
+    }else{
+        showResult();
+    }
+}
+
+function showQuetions(index){
+    const que_text = document.querySelector(".que_text");
+    let que_tag = '<span>'+ questions[index].numb + ". " + questions[index].question +'</span>';
+    let option_tag = '<div class="option"><span>'+ questions[index].options[0] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[1] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[2] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[3] +'</span></div>';
+    que_text.innerHTML = que_tag;
+    option_list.innerHTML = option_tag;
+    
+    const option = option_list.querySelectorAll(".option");
+    for(i=0; i < option.length; i++){
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }
+}
+
+let tickIconTag = '<div class="icon tick"><i class="fas fa-check"></i></div>';
+let crossIconTag = '<div class="icon cross"><i class="fas fa-times"></i></div>';
+
+function optionSelected(answer){
+    let userAns = answer.textContent;
+    let correcAns = questions[que_count].answer;
+    const allOptions = option_list.children.length;
+    
+    if(userAns == correcAns){
+        userScore += 1;
+        answer.classList.add("correct");
+        answer.insertAdjacentHTML("beforeend", tickIconTag);
+        console.log("Correct Answer");
+        console.log("Your correct answers = " + userScore);
+    }else{
+        answer.classList.add("incorrect");
+        answer.insertAdjacentHTML("beforeend", crossIconTag);
+        console.log("Wrong Answer");
+        for(i=0; i < allOptions; i++){
+            if(option_list.children[i].textContent == correcAns){
+                option_list.children[i].setAttribute("class", "option correct");
+                option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag);
+                console.log("Auto selected correct answer.");
+            }
+        }
+    }
+    for(i=0; i < allOptions; i++){
+        option_list.children[i].classList.add("disabled");
+    }
+    next_btn.classList.add("show");
+}
+function showResult(){
+    info_box.classList.remove("activeInfo");
+    quiz_box.classList.remove("activeQuiz");
+    result_box.classList.add("activeResult");
+    const scoreText = result_box.querySelector(".score_text");
+    if (userScore = 5){
+        let scoreTag = '<span>PERFECTION! You Scored <p>'+ userScore +'</p> out of <p>'+ questions.length +'!</p></span>';
+        scoreText.innerHTML = scoreTag;
+    }
+    else if (userScore > 3){
+        let scoreTag = '<span>WOW! You Scored <p>'+ userScore +'</p> out of <p>'+ questions.length +'!</p></span>';
+        scoreText.innerHTML = scoreTag;
+    }
+    else if(userScore > 1){
+        let scoreTag = '<span>Nice! You Scored <p>'+ userScore +'</p> out of <p>'+ questions.length +'!</p></span>';
+        scoreText.innerHTML = scoreTag;
+    }
+    else{
+        let scoreTag = '<span>Oh Darn! You Scored <p>'+ userScore +'</p> out of <p>'+ questions.length +'!</p></span>';
+        scoreText.innerHTML = scoreTag;
+    }
+}
+function startTimer(time){
+    counter = setInterval(timer, 1000);
+    function timer(){
+        timeCount.textContent = time;
+        time--;
+        if(time < 9){
+            let addZero = timeCount.textContent; 
+            timeCount.textContent = "0" + addZero;
+        }
+        if(time < 0){
+            clearInterval(counter);
+            timeText.textContent = "Time Gone";
+            const allOptions = option_list.children.length;
+            let correcAns = questions[que_count].answer;
+            for(i=0; i < allOptions; i++){
+                if(option_list.children[i].textContent == correcAns){
+                    option_list.children[i].setAttribute("class", "option correct");
+                    option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag);
+                    console.log("Time Off: Auto selected correct answer.");
+                }
+            }
+            for(i=0; i < allOptions; i++){
+                option_list.children[i].classList.add("disabled");
+            }
+            next_btn.classList.add("show");
+        }
+    }
+}
+
+function queCounter(index){
+    let totalQueCounTag = '<span><p>'+ index +'</p> of <p>'+ questions.length +'</p> Questions</span>';
+    bottom_ques_counter.innerHTML = totalQueCounTag;
+}
+
+let questions = [
+    {
+    numb: 1,
+    question: "Commonly used data types DO NOT include?",
+    answer: "Alerts",
+    options: [
+        "Strings",
+        "Booleans",
+        "Alerts",
+        "Numbers"
+    ]
+    },
+    {
+    numb: 2,
+    question: "The condition in an if/else statement is enclosed with?",
+    answer: "Curly Brackets",
+    options: [
+        "Quotes",
+        "Curly Brackets",
+        "Square Brackets",
+        "Parenthesis"
+    ]
+    },
+    {
+    numb: 3,
+    question: "Arrays in JS can be used to store?",
+    answer: "All of the above",
+    options: [
+        "Numbers and Strings",
+        "Other Arrays",
+        "Booleans",
+        "All of the above"
+    ]
+    },
+    {
+    numb: 4,
+    question: "String values must be enclosed within _____ when being assigned to variables.",
+    answer: "Quotes",
+    options: [
+        "Commas",
+        "Curly Brackets",
+        "Quotes",
+        "Parenthesis"
+    ]
+    },
+    {
+    numb: 5,
+    question: "A useful tool during development and debugging for printing content to the debugger is?",
+    answer: "Console.Log",
+    options: [
+        "Console.Log",
+        "JavaScript",
+        "For Loops",
+        "Terminal/Bash"
+    ]
+    },
+];
